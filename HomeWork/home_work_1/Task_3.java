cfe7142c8c43e9c14ed3ce8cbd81fef2f8a3b759
Karina <karina.dragan4uk@yandex.ru>
2022-10-18 15:09:42 +0300

    решение 3 задачи

--- HomeWork/home_work_1/Task_3.java
+++ HomeWork/home_work_1/Task_3.java
@@ -7,7 +7,28 @@ public class Task_3 {
 
     public static String reverseWords(String s) {
         s = s.trim().replaceAll(" +", " ");
-        String[] words = s.split(" ");
+        // String[] words = s.split(" ");
+        int numSpace = 0; 
+        char[] symbolArray = s.toCharArray();
+
+        for (int i = 0; i < symbolArray.length; i++) {
+            if (symbolArray[i] == ' ') {
+                numSpace++;
+            }
+        }
+
+        String temp = "";
+        String[] words = new String[numSpace + 1];
+        int index = 0;
+        for (int i = 0; i < symbolArray.length; i++) {
+             if (symbolArray[i] == ' ') {
+                words[index++] = temp;
+                temp = "";
+            } else
+                temp = temp + symbolArray[i];
+        }
+        words[index] = temp;
+        
         String[] newArray = new String[words.length];
         for (int i = 0; i < newArray.length; i++) {
             newArray[newArray.length - 1 - i] = words[i];
