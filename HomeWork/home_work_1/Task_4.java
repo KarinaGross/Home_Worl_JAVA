75c757ea5d70e0d1374c904869d5c8aa6aa8910a
Karina <karina.dragan4uk@yandex.ru>
2022-10-18 15:09:47 +0300

    решение 4 задачи

--- HomeWork/home_work_1/Task_4.java
+++ HomeWork/home_work_1/Task_4.java
@@ -1,6 +1,7 @@
+
 public class Task_4 {
     public static void main(String[] args) {
-        String s = " race a car";
+        String s = " ab@a";
         boolean result = isPalindrome(s);
         System.out.println(result);
     }
@@ -9,18 +10,23 @@ public class Task_4 {
         s = s.trim().toLowerCase();
         for (int i = 0; i < s.length(); i++) {
             int ascii = (int) s.charAt(i);
-            if (ascii >= 33 && ascii <= 47 || ascii == 58 || ascii == 59 || ascii == 63) {
+            if (ascii >= 33 && ascii <= 47 || ascii >= 58 && ascii <= 64 || ascii >= 91 && ascii <= 96 || ascii >= 123 && ascii <= 127) {
                 s = s.replace(s.charAt(i), ' ');
             }
         }
-        s = s.replaceAll(" +", "");
-        
-        // String sReversed = "";
-        // for (int i = 0; i < s.length(); i++) {
-        //     sReversed = s.charAt(i) + sReversed;
-        // }
+        // s = s.replaceAll(" +", "");
+       
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ' ) {
+                s = s.replace(" ", "");
+            }
+        }
+        // String sReversed = new StringBuilder(s).reverse().toString();
 
-        String sReversed = new StringBuilder(s).reverse().toString();
+        String sReversed = "";
+        for (int i = 0; i < s.length(); i++) {
+            sReversed = s.charAt(i) + sReversed;
+        }
 
         if (s == "") return true;
         else return s.equals(sReversed);
