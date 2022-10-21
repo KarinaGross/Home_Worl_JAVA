00d2f1defcb3b02f6b73460d314874cf28ca4e87
Karina <karina.dragan4uk@yandex.ru>
2022-10-21 17:57:48 +0300

    задача решена

---
+++ HomeWork/home_work_2/Task_01.java
@@ -0,0 +1,26 @@
+
+// import java.util.ArrayList;
+
+// public class Task_01 {
+//     public static void main() {
+        
+//     }
+
+//     // public List<List<Integer>> combine(int n, int k) {
+//     //     // ArrayList arr = new ArrayList<>();
+        
+        
+//     //     int var;
+
+//     //     if (var == n) {
+//     //         return;
+//     //     }
+
+//     //     listOfLists.add({var, var + 1});
+//     //     List<List<Integer>> listOfLists = new ArrayList();
+//     //     List<Integer> element = new ArrayList();
+//     //     arr.
+//     //     listOfLists.add(null)
+
+//     // }
+// }
