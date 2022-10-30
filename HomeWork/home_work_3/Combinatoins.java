3176db42a989564e0efe38be83d1b4cc4ae4e540
Karina <karina.dragan4uk@yandex.ru>
2022-10-30 19:59:44 +0300

    Вторая задача почти готова

---
+++ HomeWork/home_work_3/Combinatoins.java
@@ -0,0 +1,58 @@
+package home_work_3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinatoins {
+    public static void main(String[] args) {
+        Sol sol = new Sol();
+        List<List<Integer>> result = sol.combine(9, 2);
+        System.out.println(result);
+    }
+}
+
+// class Sol {
+//     List<List<Integer>> ans;
+    
+//     public List<List<Integer>> combine(int n, int k) {
+//         ans = new ArrayList<>();
+//         helper(0, new ArrayList<>(), n, k);
+//         return ans;
+//     }
+
+//     private void helper(int prev, List<Integer> comb, int n, int k) {
+//         if (comb.size() == k) {
+//             ans.add(new ArrayList<>(comb));
+//         }
+
+//         for (int i = prev + 1; i < n + 1; i++) {
+//             comb.add(i);
+//             helper(i, comb, n, k);
+//             comb.remove(comb.size() - 1);
+//         }
+//     }
+// }
+
+
+class Sol {
+    List<List<Integer>> ans;
+
+    public List<List<Integer>> combine(int n, int k) {
+        ans = new ArrayList<>();
+        helper(new ArrayList<>(), n, k);
+        return ans;
+    }
+
+    private void helper(List<Integer> comb, int n, int k) { // комбинации с повторениями
+        if (comb.size() == k) {
+            ans.add(new ArrayList<>(comb));
+            return;
+        }
+
+        for (int i = 0; i <= n; i++) {
+            comb.add(i);
+            helper(comb, n, k);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
--- HomeWork/home_work_3/Task_002.java
+++ HomeWork/home_work_3/Task_002.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Task_002 {
     static List<Integer> listIndex; 
+    static List<List<Integer>> ans;
     public static void main(String[] args) {
         String expr = "2? + ?5 = 69";
         String result = expression(expr);
@@ -12,25 +13,64 @@ public class Task_002 {
     }
 
     public static String expression(String expr) {
-        String newExpr = expr.replace(" ", "");
-        char[] arrayExpr = newExpr.toCharArray();
+        expr = expr.replace(" ", "");
+        char[] arrayExpr = expr.toCharArray();
         listIndex = new ArrayList<>();
         for (int i = 0; i < arrayExpr.length; i++) {
             if (arrayExpr[i] == '?') {
                 listIndex.add(i);
             }
+            if (arrayExpr[i] == '+') {
+                arrayExpr[i] = '=';
+            }
         }
 
+        List<List<Integer>> comb = combine(listIndex.size());
+
+        String res = "";
+        for (List<Integer> list : comb) {
+            if (chekout(arrayExpr, list)) {
+                res = "Решение есть";
+            } else {
+                res = "Решений нет";
+            }
+        }
+        return res;
     }
 
-    public static void helper(int prev, List<Integer> index, int n, int k) {
-        if (comb.size() == index.length) {
+    public static List<List<Integer>> combine(int k) {
+        ans = new ArrayList<>();
+        helper(new ArrayList<>(), k);
+        return ans;
+    }
+
+    private static void helper(List<Integer> comb, int k) { // комбинации с повторениями
+        if (comb.size() == k) {
             ans.add(new ArrayList<>(comb));
+            return;
         }
 
-        for (int i = prev + 1; i < n + 1; i++) {
+        for (int i = 0; i <= 9; i++) {
             comb.add(i);
-            helper(i, comb, n, k);
+            helper(comb, k);
             comb.remove(comb.size() - 1);
+        }
+    }
+
+    public static boolean chekout(char[] exprchar, List<Integer> comb) {
+        for (int i = 0; i < listIndex.size(); i++) {
+            int num = comb.get(i);
+            exprchar[listIndex.get(i)] = (char) num;
+        } 
+        String newexpr = String.valueOf(exprchar);
+        String[] exprarray = newexpr.split("=");
+
+        List<Integer> numbers = new ArrayList<>();
+        for (String str : exprarray) {
+            int num = Integer.parseInt(str);
+            numbers.add(num);
+        }
+
+        return numbers.get(0) + numbers.get(1) == numbers.get(2);
     }
 }
