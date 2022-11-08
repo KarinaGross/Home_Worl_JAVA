558d70c7203142c57edaae289836a1b47d9b7c4b
Karina <karina.dragan4uk@yandex.ru>
2022-11-08 15:57:03 +0300

    Перестановки переделала в комбинации

---
+++ HomeWork/home_work3/Combinations.java
@@ -0,0 +1,35 @@
+package home_work3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations {
+    public static void main(String[] args) {
+        Sol sol = new Sol();
+        List<List<Integer>> result = sol.combine(9, 2);
+        System.out.println(result);
+    }
+}
+
+class Sol {
+    List<List<Integer>> ans;
+
+    public List<List<Integer>> combine(int n, int k) {
+        ans = new ArrayList<>();
+        helper(new ArrayList<>(), n, k);
+        return ans;
+    }
+
+    private void helper(List<Integer> comb, int n, int k) { // комбинации с повторениями
+        if (comb.size() == k) {
+            ans.add(new ArrayList<>(comb));
+            return;
+        }
+
+        for (int i = 0; i <= n; i++) {
+            comb.add(i);
+            helper(comb, n, k);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
